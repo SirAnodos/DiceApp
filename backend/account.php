172fdb17ba8bcd1cd73be3884529c2eda7cbc375
= <=>
2025-04-21 10:34:25 -0400

    login and register account

--- backend/account.php
+++ backend/account.php
@@ -2,23 +2,22 @@
 $uname = $_POST['uname'] ?? null;
 $pwd = $_POST['pwd'] ?? null;
 $action = $_POST['action'];
-include(db-connect.php);
+include('db-connect.php');
 
 if ($action == 'login') {
-    $qry = $connection->prepare('SELECT id, password FROM users WHERE username = ?');
+    $qry = $connection->prepare("SELECT id, password FROM users WHERE username = ?");
     $qry->bind_param('s', $uname);
     $qry->execute();
     $qry->store_result();
 
     if ($qry->num_rows === 1) {
-        $qry->bind_result($userID, $pwdHash);
+        $qry->bind_result($id, $pwdHash);
         $qry->fetch();
 
         if (password_verify($pwd, $pwdHash)) {
             session_start();
-            $_SESSION['user_id'] = $userID;
-            $_SESSION['username'] = $uname;
-
+            $_SESSION['id'] = $id;
+            $_SESSION['uname'] = $uname;
             header('HTTP/1.1 200');
         } else {
             header('HTTP/1.1 401');
@@ -33,6 +32,22 @@ if ($action == 'logout') {
 } else
 
 if ($action == 'register') {
-    header('HTTP/1.1 200');
+    $qry = $connection->prepare("SELECT id FROM users WHERE username = ?");
+    $qry->bind_param('s', $uname);
+    $qry->execute();
+    $qry->store_result();
+
+    if ($qry->num_rows === 0) {
+        $qry = $connection->prepare("INSERT INTO `users` (`id`, `username`, `password`) VALUES (NULL, '?', '?')");
+        $qry->bind_param('ss', $uname, password_hash($pwd));
+        $qry->execute();
+        $id = $connection->insert_id;
+        header('HTTP/1.1 200');
+        session_start();
+        $_SESSION['id'] = $id;
+        $_SESSION['uname'] = $uname;
+    } else {
+        header('HTTP/1.1 409');
+    }
 }
 ?>
