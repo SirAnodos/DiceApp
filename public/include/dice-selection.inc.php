b081af22cfd1df332101d76026f8dd52d9343d65
= <walkergabe13@gmail.com>
2025-05-10 21:47:41 -0400

    refactoring

--- public/include/dice-selection.inc.php
+++ public/include/dice-selection.inc.php
@@ -1,32 +1,35 @@
 <!-- Dice Selection Form -->
 
 <?php
+// Different elements between the roller and statistics pages.
+if ($activePage=='statistics') {
+  $toHitInput = "
+    <span>to hit:</span>
+    <input id='to-hit' type='number' name='to-hit' value='$toHit' min=0 autocomplete='off'>
+    <br>";
+  $submitButton = "Stats";
+} else if ($activePage=='roller') {
+  $toHitInput = "";
+  $submitButton = "Roll";
+}
+
 echo "<form class='input-panel' method='GET' action='".htmlspecialchars($_SERVER["PHP_SELF"])."'>";
   // echo one input for each kind of die
   foreach (DICEMAP as $sides => $idx) {
     echo "<span>d" . $sides . "s:</span>";
     echo "<input id='d" . $sides . "-input' type='number' name='d$sides'
-        value='$dice[$sides]' min=0 max=MAXDICE autocomplete='off'";
-    if ($activePage == 'statistics') {
-        echo "onchange='validateSelection(this, this.value)'>";
-    } else if ($activePage == 'roller') {
-        echo "onchange='updateDice($sides)' autocomplete='off'>";
-    }
+        value='$dice[$sides]' min=0 max=MAXDICE autocomplete='off' ";
+        if ($activePage=='statistics') {echo "onchange='validateSelection(this, this.value)'>";}
+        else if ($activePage=='roller') {echo "onchange='updateDice($sides)'>";}
     // line break after every 2 inputs
     if ($idx % 2 != 0) {
       echo "<br>";
     }
   }
-  if ($activePage=="statistics") {
-    echo "
-    <span>to hit:</span>
-    <input id='to-hit' type='number' name='to-hit' value='$toHit' min=0 autocomplete='off'>
-    <br>";
-  }
+  // minimum to hit
+  echo $toHitInput;
+  // reset or submit form
   echo "
   <button type='submit' onclick='resetForm(this.parentElement)'>Reset</button>
-  <button type='submit'>";
-  if ($activePage == 'statistics') { echo "Stats";}
-  else if ($activePage=='roller') {echo "Roll";}
-  echo "</button><br>
+  <button type='submit'>".$submitButton."</button><br>
 </form>";
